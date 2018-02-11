2a5df55d3c7de69175779224adb835e0e0ef94bb
morpheus65535 <5130500+morpheus65535@users.noreply.github.com>
2018-02-11 14:23:12 -0500

    Scheduler bug correction if auto update enable

--- scheduler.py
+++ scheduler.py
@@ -8,7 +8,7 @@ if automatic == 'True':
     scheduler.add_job(check_and_apply_update, 'interval', hours=6, max_instances=1, coalesce=True, misfire_grace_time=15, id='update_bazarr', name='Update bazarr from source on Github')
 else:
     scheduler.add_job(check_and_apply_update, 'cron', year='2100', hour=4, id='update_bazarr', name='Update bazarr from source on Github')
-    scheduler.start()
+scheduler.start()
 
 def execute_now(taskid):
     scheduler.modify_job(taskid, jobstore=None, next_run_time=datetime.now())
