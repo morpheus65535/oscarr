e68f02e7af8e289c7c7b460d08f955741fa4f6b5
morpheus65535 <5130500+morpheus65535@users.noreply.github.com>
2018-05-01 22:41:52 -0400

    Rework update module routine

--- update_modules.py
+++ update_modules.py
@@ -1,6 +1,28 @@
-import pip
+import subprocess
+from subprocess import check_output
+import logging
+import os
+import sys
 
-#try:
-#    pip.main(['install', '--user', 'gitpython'])
-#except SystemExit as e:
-#    pass
+try:
+    logging.info('Installing Python modules required for Oscarr...')
+
+    command = sys.executable + ' -m pip --disable-pip-version-check -q -q install --user -r ' + os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+    if os.name == 'nt':
+        codepage = check_output("chcp", shell=True, stderr=subprocess.STDOUT)
+        encoding = codepage.split(':')[-1].strip()
+
+    process = check_output(command, shell=True, stderr=subprocess.STDOUT)
+
+    if os.name == 'nt':
+        process = process.decode(encoding)
+except:
+    logging.error('Unable to install requirements using command line PIP. Is PIP installed and included in system path?')
+    pass
+else:
+    if process == "":
+        logging.info('Required Python modules installed if missing.')
+    else:
+        for line in process.splitlines():
+            logging.error(line)
